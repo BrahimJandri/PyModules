9697e2976dfbf570abec8093592da3e147b1c0e0
BrahimJandri <bzaza066@gmail.com>
2026-03-06 15:29:59 +0000

    create the first exercies

---
+++ module00/ft_hello_garden.py
@@ -0,0 +1,7 @@
+def ft_hello_garden():
+    print("Hello, Garden Community!")
+
+# we write the main function to test our code, but it will not be called when we import this module in another file
+
+if __name__ == "__main__":
+    ft_hello_garden()
